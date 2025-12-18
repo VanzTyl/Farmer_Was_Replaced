cd91252013b7d8f0d7ca8fd9581a41b1b144d9ff
VanzTyl <125661587+VanzTyl@users.noreply.github.com>
2025-12-19 03:55:10 +0800

    Create reset_position.py

---
+++ Utilities/reset_position.py
@@ -0,0 +1,20 @@
+n = North
+s = South
+e = East
+w = West
+
+# Change the char to your direction
+
+def move_to_origin():
+	while get_pos_x() > 0:
+		if get_pos_x() > get_world_size() / 2:
+			move(e)
+		else:
+			move(w)
+	while get_pos_y() > 0:
+		if get_pos_y() > get_world_size() / 2:
+			move(n)
+		else:
+			move(s)
+
+move_to_origin()
